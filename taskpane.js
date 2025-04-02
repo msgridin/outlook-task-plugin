7d20e26236c6a2c6e53937bc8c2993b858e9d424
Maksim Gridin <msg@iMac-Maksim.local>
2025-04-02 16:59:31 +0300

    .

--- taskpane.js
+++ taskpane.js
@@ -1,6 +1,6 @@
 Office.onReady(() => {
     window.addEventListener("DOMContentLoaded", () => {
-        const btn = document.getElementById("sendButton");
+        const btn = document.getElementById("createTaskButton");
         if (btn) {
             btn.addEventListener("click", () => {
                 console.log("Send button clicked");
